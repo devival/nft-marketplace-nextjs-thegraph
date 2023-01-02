c5adce17d74a98fe910fd104abac25a57fe449d9
BUK <BUK_@MacBook-Pro-V.local>
2023-01-02 19:53:15 +0100

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+import { NotificationProvider } from "web3uikit"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+})
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <ApolloProvider client={client}>
+                    <NotificationProvider>
+                        <Header />
+                        <Component {...pageProps} />
+                    </NotificationProvider>
+                </ApolloProvider>
+            </MoralisProvider>
+        </div>
+    )
+}
